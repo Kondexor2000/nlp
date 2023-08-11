af42423343cad49bae0c277271fe3c08c8c0b6b2
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2023-08-11 15:56:30 +0200

    Add files via upload

---
+++ nlp_2_modyfikowany.py
@@ -0,0 +1,55 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.probability import FreqDist
+import string
+from textblob import TextBlob
+from langdetect import detect
+
+# Pobieranie przykładowego tekstu (możesz też podać swój własny tekst)
+nltk.download('punkt')
+nltk.download('stopwords')
+text = input()
+
+# Rozpoznawanie języka
+detected_language = detect(text)
+
+# Tworzenie obiektu TextBlob
+blob = TextBlob(text)
+
+# Wyświetlenie sentymentu (polarity -1 do 1, subjectivity 0 do 1)
+sentiment = blob.sentiment
+print(f"Sentyment: Polarity = {sentiment.polarity:.2f}, Subjectivity = {sentiment.subjectivity:.2f}")
+
+# Klasyfikacja sentymentu na podstawie polarity
+if sentiment.polarity > 0:
+    emocje = "pozytywny"
+elif sentiment.polarity < 0:
+    emocje = "negatywny"
+else:
+    emocje = "neutralny"
+
+if detected_language == "en":
+    if emocje != "negatywny":  
+        # Tokenizacja tekstu na zdania i słowa
+        sentences = sent_tokenize(text)
+        words = word_tokenize(text)
+
+        # Usuwanie znaków interpunkcyjnych i przekształcanie na małe litery
+        cleaned_words = [word.lower() for word in words if word.isalnum()]
+
+        # Usuwanie stop words (słów nieistotnych) z tekstu
+        stop_words = set(stopwords.words('english'))
+        filtered_words = [word for word in cleaned_words if word not in stop_words]
+
+        # Analiza częstości występowania słów
+        freq_dist = FreqDist(filtered_words)
+
+        # Wyświetlanie najczęściej występujących słów
+        print("Najczęściej występujące słowa:")
+        for word, frequency in freq_dist.most_common(10):
+            print(f"{word}: {frequency}")
+    else:
+        print("Tekst nie jest na miejscu!")
+else:
+    print("Sorry! I don't understand")
